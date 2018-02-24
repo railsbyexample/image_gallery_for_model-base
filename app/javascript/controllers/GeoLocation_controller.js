f6a859ec5ba3d2ed31615b5b26dde86b2783ff6f
Santiago Perez Perret <perezperret@gmail.com>
2018-02-24 15:08:20 -0400

    use geo_location label existing value in geo_location input

--- app/javascript/controllers/GeoLocation_controller.js
+++ app/javascript/controllers/GeoLocation_controller.js
@@ -4,14 +4,15 @@ export default class extends Controller {
   static targets = [
     'autocomplete',
     'placeId',
-    'label',
     'latitude',
     'longitude',
     'addressComponents'
   ];
 
   connect() {
+    // Initialize Google Place autocomplete on label input
     this.autocomplete = new google.maps.places.Autocomplete(this.autocompleteTarget);
+    // Add listener to react to place change
     this.autocomplete.addListener('place_changed', () => { this.change() })
   }
 
@@ -21,7 +22,6 @@ export default class extends Controller {
 
   populateModel(place) {
     this.placeIdTarget.value = place.place_id;
-    this.labelTarget.value = place.formatted_address;
     this.latitudeTarget.value = place.geometry.location.lat();
     this.longitudeTarget.value = place.geometry.location.lng();
     this.addressComponentsTarget.value = JSON.stringify(place.address_components);
