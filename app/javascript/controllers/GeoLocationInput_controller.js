601dfd84c177d149902e706d44d9a550d4317520
Santiago Perez Perret <perezperret@gmail.com>
2018-02-27 20:51:30 -0400

    send geo_location_attributes as string in geo_location_input

--- app/javascript/controllers/GeoLocationInput_controller.js
+++ app/javascript/controllers/GeoLocationInput_controller.js
@@ -3,33 +3,37 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = [
     'autocomplete',
-    'placeId',
-    'latitude',
-    'longitude',
-    'addressComponents'
+    'geoLocationInput'
   ];
 
   connect() {
     // Initialize Google Place autocomplete on label input
     this.autocomplete = new google.maps.places.Autocomplete(this.autocompleteTarget);
     // Add listener to react to place change
-    this.autocomplete.addListener('place_changed', () => { this.change() })
+    this.autocomplete.addListener('place_changed', () => { this.updateModel(this.autocomplete.getPlace()) });
     // Allow enter to select suggestion on Google Place autocomplete
-    google.maps.event.addDomListener(this.autocompleteTarget, 'keydown', event => {
-      if (event.keyCode == 13 && $('.pac-container:visible').length) {
-        event.preventDefault();
+    google.maps.event.addDomListener(this.autocompleteTarget, 'keydown', this.allowSelectOnEnter);
+  }
+
+  updateModel = (place) => {
+    let geoLocation = ''
+
+    if (place.place_id) {
+      geoLocation = {
+        label: place.label,
+        place_id: place.place_id,
+        latitude: place.geometry.location.lat(),
+        longitude: place.geometry.location.lng(),
+        address_components: place.address_components
       }
-    });
+    }
+
+    this.geoLocationInputTarget.value = JSON.stringify(geoLocation)
   }
 
-  change() {
-    this.populateModel(this.autocomplete.getPlace());
-  }
-
-  populateModel(place) {
-    this.placeIdTarget.value = place.place_id;
-    this.latitudeTarget.value = place.geometry.location.lat();
-    this.longitudeTarget.value = place.geometry.location.lng();
-    this.addressComponentsTarget.value = JSON.stringify(place.address_components);
+  allowSelectOnEnter = event => {
+    if (event.keyCode == 13 && $('.pac-container:visible').length) {
+      event.preventDefault();
+    }
   }
 }
