86e55e346a5f19b195777cfefb8a82b64d5e1a4c
Santiago Perez Perret <perezperret@gmail.com>
2018-02-28 12:17:27 -0400

    move property params to a concern

---
+++ app/controllers/concerns/property_params.rb
@@ -0,0 +1,33 @@
+##
+# Global application helpers
+module PropertyParams
+  def property_params
+    transformed_params.require(:property).permit(
+      :title, :price_per_month, :number_of_rooms, :number_of_bathrooms,
+      :size_in_square_meters, :description, { features: [] },
+      geo_location_attributes: [
+        :place_id, :label, :latitude, :longitude,
+        address_components: [:long_name, :short_name, types: []]
+      ]
+    )
+  end
+
+  def transformed_params
+    transformed_params = params.dup
+
+    transformed_params = transform__property_json(transformed_params, :features)
+    transformed_params = transform__property_json(transformed_params, :geo_location_attributes)
+
+    transformed_params
+  end
+
+  def transform__property_json(params, attribute)
+    transformed_params = params
+
+    if params.dig(:property, attribute)
+      transformed_params[:property][attribute] = JSON.parse(params[:property][attribute])
+    end
+
+    transformed_params
+  end
+end
