d2999112b7d7553b9098723abd3136fadaf8d54e
Santiago Perez Perret <perezperret@gmail.com>
2018-04-17 08:27:23 -0400

    add property cards

---
+++ app/helpers/properties_helper.rb
@@ -0,0 +1,19 @@
+##
+# Helpers for properties views
+module PropertiesHelper
+  def cover_url(property)
+    if property.images.count.zero?
+      'https://placehold.it/400x300'
+    else
+      attachment_url(property.images.first, :attached_file)
+    end
+  end
+
+  def size(property)
+    "#{property.size_in_square_meters.to_i}m2"
+  end
+
+  def price(property)
+    "#{money_without_cents_and_with_symbol(property.price_per_month)}/mo"
+  end
+end
